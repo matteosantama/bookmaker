e5343dbae36267331b03fd1fe1ef03530339da80
Matteo Santamaria <matteosantama@gmail.com>
2020-10-22 14:57:49 -0700

    Odds data now contains NBA gameid/teamid info, so we can easily match up games for evaluation

--- preprocessors/oddsprocessor.py
+++ preprocessors/oddsprocessor.py
@@ -1,40 +1,44 @@
 import argparse
 import os
+import re
 
 import pandas as pd
 
 
+# Mapping to canonical team names
 teams = {
-    'Atlanta':      'Atlanta Hawks',
-    'Boston':       'Boston Celtics',
-    'Brooklyn':     'Brooklyn Nets',
-    'Charlotte':    'Charlotte Hornets',
-    'Chicago':      'Chicago Bulls',
-    'Cleveland':    'Cleveland Cavaliers',
-    'Dallas':       'Dallas Mavericks',
-    'Denver':       'Denver Nuggets',
-    'Detroit':      'Detroit Pistons',
-    'GoldenState':  'Golden State Warriors',
-    'Houston':      'Houston Rockets',
-    'Indiana':      'Indiana Pacers',
-    'LAClippers':   'Los Angeles Clippers',
-    'LALakers':     'Los Angeles Lakers',
-    'Memphis':      'Memphis Grizzlies',
-    'Miami':        'Miami Heat',
-    'Milwaukee':    'Milwaukee Bucks',
-    'Minnesota':    'Minnesota Timberwolves',
-    'NewOrleans':   'New Orleans Pelicans',
-    'NewYork':      'New York Knicks',
-    'OklahomaCity': 'Oklahoma City Thunder',
-    'Orlando':      'Orlando Magic',
-    'Philadelphia': 'Philadelphia 76ers',
-    'Phoenix':      'Phoenix Suns',
-    'Portland':     'Portland Trail Blazers',
-    'Sacramento':   'Sacramento Kings',
-    'SanAntonio':   'San Antonio Spurs',
-    'Toronto':      'Toronto Raptors',
-    'Utah':         'Utah Jazz',
-    'Washington':   'Washington Wizards',
+    'Atlanta':          'Atlanta Hawks',
+    'Boston':           'Boston Celtics',
+    'Brooklyn':         'Brooklyn Nets',
+    'Charlotte':        'Charlotte Hornets',
+    'Chicago':          'Chicago Bulls',
+    'Cleveland':        'Cleveland Cavaliers',
+    'Dallas':           'Dallas Mavericks',
+    'Denver':           'Denver Nuggets',
+    'Detroit':          'Detroit Pistons',
+    'GoldenState':      'Golden State Warriors',
+    'Houston':          'Houston Rockets',
+    'Indiana':          'Indiana Pacers',
+    'LAClippers':       'Los Angeles Clippers',
+    'LA Clippers':      'Los Angeles Clippers',
+    'LALakers':         'Los Angeles Lakers',
+    'Memphis':          'Memphis Grizzlies',
+    'Miami':            'Miami Heat',
+    'Milwaukee':        'Milwaukee Bucks',
+    'Minnesota':        'Minnesota Timberwolves',
+    'NewOrleans':       'New Orleans Pelicans',
+    'NewYork':          'New York Knicks',
+    'OklahomaCity':     'Oklahoma City Thunder',
+    'Oklahoma City':    'Oklahoma City Thunder',
+    'Orlando':          'Orlando Magic',
+    'Philadelphia':     'Philadelphia 76ers',
+    'Phoenix':          'Phoenix Suns',
+    'Portland':         'Portland Trail Blazers',
+    'Sacramento':       'Sacramento Kings',
+    'SanAntonio':       'San Antonio Spurs',
+    'Toronto':          'Toronto Raptors',
+    'Utah':             'Utah Jazz',
+    'Washington':       'Washington Wizards',
 }
 
 
@@ -44,86 +48,100 @@ def process_and_write(year: int, domain: str, verbose: bool) -> None:
     """
     inpath = os.path.join(
         '..', 'data', 'raw', f'odds-{year - 1}-{year - 2000}.xlsx')
-    df = pd.read_excel(inpath)
-        
-    # First we fix the year in the df
+    raw = pd.read_excel(inpath)
+    # First we have to fix the date because no year info is included
     if verbose:
         print('\tParsing monthyear dates')
         
     new_year = False
-    for i, monthday in df['Date'].iteritems():
+    for i, monthday in raw['Date'].iteritems():
         stringified = str(monthday)
-        day = int(stringified[:-2])
+        day = int(stringified[-2:])
         month = int(stringified[:-2])
         if month == 1:
             new_year = True
         ts_year = year if new_year else year - 1
         ts = pd.Timestamp(year=ts_year, month=month, day=day)
-        df.at[i, 'Date'] = ts
+        raw.at[i, 'Date'] = ts
 
-    # Next we fix the team names
+    # Next we standarize the team names
     if verbose:
         print('\tUpdating team names')
         
-    df['Team'] = df['Team'].map(teams)
+    raw['Team'] = raw['Team'].map(teams)
+   
+    # If we read these columns in as strings, we need to replace and convert
+    regex = re.compile('pk', flags=re.I)
+    raw[['Open', 'Close']] = raw[['Open', 'Close']].replace(
+            regex, 0., regex=True)
 
-    cleaned = pd.DataFrame(index=pd.RangeIndex(len(df) // 2), columns=[
-        'date', 'home', 'away', 'home_score', 'away_score', 'ou_open',
-        'ou_close', 'home_ml', 'away_ml', 'spread_open', 'spread_close'
+    df = pd.DataFrame(index=pd.RangeIndex(len(raw)), columns=[
+        'GAME_DATE', 'TEAM_NAME', 'pts', 'ou_open',
+        'ou_close', 'ml', 'spread_open', 'spread_close'
     ])
-    for i in range(len(df) // 2):
-        # Select our two teams
-        a = df.iloc[2 * i]
-        b = df.iloc[2 * i + 1]
-        assert a.at['Date'] == b.at['Date']
+    for i in range(0, len(df), 2):
+        # Select two teams at a time
+        row_a = raw.iloc[i]
+        row_b = raw.iloc[i + 1]
+        assert row_a.at['Date'] == row_b.at['Date']
 
-        home = a if a.at['VH'] == 'H' else b
-        away = b if b.at['VH'] == 'V' else a
+        home = row_a if row_a.at['VH'] == 'H' else row_b
+        away = row_b if row_b.at['VH'] == 'V' else row_a
         assert not home.equals(away)
 
-        # Manually construct `cleaned` ...blgh
-        cleaned.at[i, 'date'] = home.at['Date']
-        cleaned.at[i, 'home'] = home.at['Team']
-        cleaned.at[i, 'away'] = away.at['Team']
-        cleaned.at[i, 'home_score'] = int(home.at['Final'])
-        cleaned.at[i, 'away_score'] = int(away.at['Final'])
-        cleaned.at[i, 'away_ml'] = int(away.at['ML'])
-        cleaned.at[i, 'home_ml'] = int(home.at['ML'])
-            
-        pk = ('pk', 'PK')
-        hopen = 0 if home.at['Open'] in pk else float(home.at['Open'])
-        aopen = 0 if away.at['Open'] in pk else float(away.at['Open'])
-        hclose = 0 if home.at['Close'] in pk else float(home.at['Close'])
-        aclose = 0 if away.at['Close'] in pk else float(away.at['Close'])
+        h_open, h_close = home.at['Open'], home.at['Close']
+        a_open, a_close = away.at['Open'], away.at['Close']
 
         # In terms of structure, the odds files are a mess.
         # We have to do some smart guesswork to pull all the odds
-        ou_open = max(hopen, aopen)
-        ou_close = max(hclose, aclose)
+        ou_open = max(h_open, a_open)
+        ou_close = max(h_close, a_close)
 
-        spread_open = min(hopen, aopen)
-        spread_close = min(hclose, aclose)
+        spread_open = min(h_open, a_open)
+        spread_close = min(h_close, a_close)
 
-        cleaned.at[i, 'ou_open'] = ou_open
-        cleaned.at[i, 'ou_close'] = ou_close 
-        cleaned.at[i, 'spread_open'] = spread_open
-        cleaned.at[i, 'spread_close'] = spread_close 
+        home_is_fav = home['ML'] <= away['ML']
 
-    # Can never have too many sanity checks
-    open_comparison = cleaned['spread_open'] < cleaned['ou_open']
-    spread_open_na = cleaned['spread_open'].isnull()
-    ou_open_na = cleaned['ou_open'].isnull()
-    assert (open_comparison | spread_open_na | ou_open_na).all()
+        for offset, team in enumerate([home, away]):
+            df.at[i + offset, 'GAME_DATE'] = team['Date']
+            df.at[i + offset, 'TEAM_NAME'] = team['Team']
+            df.at[i + offset, 'pts'] = team['Final']
+            df.at[i + offset, 'ml'] = team['ML']
+            df.at[i + offset, 'ou_open'] = ou_open
+            df.at[i + offset, 'ou_close'] = ou_close
+            df.at[i + offset, 'spread_open'] = spread_open
+            df.at[i + offset, 'spread_close'] = spread_close 
+            # Spreads need to take into account who the favorite is
+            if team.equals(home) and home_is_fav:
+                df.at[i + offset, 'spread_open'] *= -1
+                df.at[i + offset, 'spread_close'] *= -1
 
-    close_comparison = cleaned['spread_close'] < cleaned['ou_close']
-    spread_close_na = cleaned['spread_close'].isnull()
-    ou_close_na = cleaned['ou_close'].isnull()
-    assert (close_comparison | spread_close_na | ou_close_na).all()
+    # At this point, the odds data has been cleaned. Now we need to 
+    # add NBA game and team ids
+    schedulefilename = f'{year}-nba-schedule.csv'
+    schedulepath = os.path.join('..', 'data', 'raw', schedulefilename)
+    schedule = pd.read_csv(
+            schedulepath, index_col=0, parse_dates=['GAME_DATE'])
+    # In 2016, the NBA calls them the 'LA Clippers', so we want to
+    # map that to the full name as well
+    schedule['TEAM_NAME'] = schedule['TEAM_NAME'].apply(
+            lambda x: x if x in teams.values() else teams[x])
     
-    assert len(cleaned) * 2 == len(df)
+    if verbose:
+        print('\tJoining NBA gameid and teamid')
+    join_keys = ['TEAM_NAME', 'GAME_DATE']
+    schedule = schedule.set_index(join_keys)
+    schedule = schedule[['GAME_ID', 'TEAM_ID']]
+
+    df = df.set_index(join_keys)
+    # We only keep odds that are in the schedule file
+    df = df.join(schedule, how='right')
+    # Make sure we didn't lose a team along the way
+    for k, v in teams.items():
+        assert v in df.index.get_level_values(0), f'MISSING {v} in {year}'
 
     outpath = os.path.join('..', 'data', domain, f'{year}-odds.csv')
-    cleaned.to_csv(outpath)
+    df.to_csv(outpath)
     if verbose:
         print(f'\t{outpath} written')
 
@@ -133,15 +151,17 @@ if __name__=='__main__':
         description='Process Excel files from Sports Book Review Online.'
     )
 
-    parser.add_argument('year', 
-        help=('Seasons are identified by their ending year.'), type=int)
-
     parser.add_argument('domain', choices=['train', 'dev', 'test'],
         help='Whether to store in data/train/, data/dev/,  or data/test/')
+    
+    parser.add_argument('--years', type=int, choices=range(2015, 2021), 
+            dest='years', nargs='*', help=('Seasons are identified by their '
+            'ending year. You can specify multiple 4 digit years'))
 
     parser.add_argument('-v', dest='verbose',
         help='Flag to display verbose output', action='store_true')
     
     args = parser.parse_args()
-
-    process_and_write(args.year, args.domain, args.verbose)
+    
+    for year in args.years:
+        process_and_write(year, args.domain, args.verbose)
